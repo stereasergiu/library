d3dcb64eeb596b1fb600e9f29a80cfa74b1d8a3a
sergiu <stereasergiu@yahoo.com>
2021-02-06 16:40:22 +0200

    created microservices

---
+++ author-service/src/test/java/com/myexample/authorservice/AuthorServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myexample.authorservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AuthorServiceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
